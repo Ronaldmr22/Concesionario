e54d96f9774066fd5153a417dd412021acee85b3
Ronaldmr22 <ronaldmorar22@gmail.com>
2026-08-19 20:45:13 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        CuentaBancaria fariathna = new CuentaBancaria(1200000);
+        fariathna.Depositar(5000);
+
+        Console.WriteLine(fariathna.Retirar(120000000));
+    }
+}
